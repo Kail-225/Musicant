d1c3c696be63713fc863576c4a37f14b6fe4596a
Kail-225 <bobyybobin@gmail.com>
2025-06-28 02:07:03 +0300

    Стандартный вид

--- sound.py
+++ sound.py
@@ -7,11 +7,11 @@ locale.setlocale(locale.LC_ALL, 'ru_RU.utf8')
 Play=True
 vc=None
 id_t=0
-Traks=["Royalty1","Прощание1","Faster_n_harder1","Rave1","Dare1","Children_of_the_sky1"]
-jsounds=["Suzume2","Shinunoga_E-wa2"]
-covers=["Kiss_of_death1","Katyusha1","Suzume1","Hope_Is_the_Thing_with_Feathers1","Idol1","Starfall1","White_Night1","Wildfire1","Shinunoga_E-wa1","La_vaguelette1"]
-gsounds=["Earth1","La_vaguelette2","Honor_for_all1","Take_the_Journey1","Rogue_Main_Theme1"]
-ssounds=["Main_Title1","Janissary_Song_11","Janissary_Song_21","Deepest_Love1"]
+Traks=["Royalty","Прощание","Faster_n_harder","Rave","Dare","Children_of_the_sky"]
+jsounds=["Suzume","Shinunoga_E-wa"]
+covers=["Kiss_of_death","Katyusha","Suzume","Hope_Is_the_Thing_with_Feathers","Idol","Starfall","White_Night","Wildfire","Shinunoga_E-wa","La_vaguelette"]
+gsounds=["Earth","La_vaguelette","Honor_for_all","Take_the_Journey","Rogue_Main_Theme"]
+ssounds=["Main_Title","Janissary_Song_1","Janissary_Song_2","Deepest_Love"]
 path=player()
 async def sound(a,bot):
     global Play, id_t
